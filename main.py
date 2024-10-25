12361d57802a77587bd72d450327436c00f1a266
ArthurDuarte77 <bolivarartur77yt@gmail.com>
2024-10-25 16:11:59 -0300

    alteração no schedule

--- main.py
+++ main.py
@@ -115,12 +115,7 @@ data_ontem = ontem.strftime("%Y-%m-%d")
 
 
 
-schedule.every().day.at("07:50").do(chamar_script(data_ontem, data_ontem, cookie=cookie))
-schedule.every().day.at("10:50").do(chamar_script(data_ontem, data_ontem, cookie=cookie))
 schedule.every().day.at("13:50").do(chamar_script(data_ontem, data_ontem, cookie=cookie))
-schedule.every().day.at("15:50").do(chamar_script(data_ontem, data_ontem, cookie=cookie))
-schedule.every().day.at("19:50").do(chamar_script(data_ontem, data_ontem, cookie=cookie))
-schedule.every().day.at("23:50").do(chamar_script(data_ontem, data_ontem, cookie=cookie))
 
 while True:
     schedule.run_pending()
